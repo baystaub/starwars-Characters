873f96b9006aeed9a77783d3c58acfb1fc76d08c
Bailey Staub <Staubbm@gmail.com>
2021-04-22 21:01:26 -0600

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // Sets up the Express App
 
 const app = express();
-const PORT = 8000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -83,4 +83,8 @@ app.post('/api/characters', (req, res) => {
 
 // Starts the server to begin listening
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, function() {
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/index.html"));
+    })
+});
